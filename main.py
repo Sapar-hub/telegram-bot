941d86e07e81becb560ccb0e0a594dd2cf92bf27
Saparmyrat <saparmyrat.saparmyradov@gmail.com>
2025-05-21 16:18:19 +0300

    added google api to access calendar

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 import telebot
+import json
 from datetime import datetime
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
 # Get your bot token from Railway environment variables
 TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
+
+# Load credentials from environment variables
+credentials_info = json.loads(os.environ['GOOGLE_CREDENTIALS'])
+token_info = json.loads(os.environ['GOOGLE_TOKEN'])
+
+# Create Credentials object
+creds = Credentials.from_authorized_user_info(token_info)
+
+# Build the service
+service = build('calendar', 'v3', credentials=creds)
 
 @bot.message_handler(commands=['start'])
 def start(message):
